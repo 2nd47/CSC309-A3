f5ba5c880e9eb9efd862d098bbe8391a8264cde9
2nd47 <2nd4777@gmail.com>
2016-07-10 11:59:16 -0400

    A3 Submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,103 +5,278 @@
 function renderBasicArticles(articles) {
   var data = $.parseJSON(articles);
   var $result = $("<div>");
-  $result.addClass('article-container');
+  $result.addClass('content-container');
 
   var article;
+  // Iterate over all of the articles and collect any relevant data
   $.each(data, function(i, item) {
     var $article = $("<div>");
-    $article.addClass('article');
+    $article.addClass('content');
+    var $title = $("<p>");
+    $title.append(item['title']).css('font-weight', 'bold');
     var $date = $("<p>");
-    console.log($date);
-    $date.append(item['published_date']);
-    $article.append($date);
+    $date.append(item['published_date']).css('font-size', 'small');
+    var $abstract = $("<p>");
+    $abstract.append(item['abstract']);
+    var $short_url = $("<a>");
+    $short_url.append(item['short_url']).attr('href', item['short_url']);
+    // Finally append all that data to the article object
+    $article.append($title, $date, $abstract, $short_url);
+    // And that to the final result
     $result.append($article);
   });
 
-  console.log($result.html());
-  $("#main").append($result);
+  $("#main").html($result);
 
   return $result;
 }
 
 // Given a list of tags and their occurrences, render them into html
 function renderTags(tags) {
-  var $result;
+  var data = $.parseJSON(tags);
+  var $result = $("<div>");
+  $result.addClass('content-container');
+
+  $title = $("<p>").append('Tag cloud (bigger means more frequent)')
+    .addClass('bold')
+  $result.append($title);
+
+  // Set up various font size configurations
+  var fontMin = 16;
+  var fontMax = 32;
+
+  // Set up the range of values needed to represent for our fonts
+  var mostOccurrences = 1;
+  var leastOccurrences = 1;
+  var rangeOccurrences;
+
+  // Just get the largest number of occurrences for any given tag
+  $.each(data, function(tagName, tagValue) {
+    if (tagValue > mostOccurrences) {
+      mostOccurrences = tagValue;
+    }
+    // This generally shouldn't happen but if a tag was never used it would work
+    else if (tagValue < leastOccurrences) {
+      leastOccurrences = tagValue;
+    }
+  });
+
+  // Get how many different states there should be between font sizes
+  rangeOccurrences = mostOccurrences - leastOccurrences;
+  // Our font increase corresponds to the range of font sizes and occurrences
+  var fontIncr = (fontMax - fontMin) / rangeOccurrences;
+
+  // Create a <p> container for all the tags and modify font-size by span style
+  $tags = $("<p>").addClass('content')
+    .css('text-align', 'center').css('margin-top', '0em');
+  $.each(data, function(tagName, tagValue) {
+    $newTag = $("<span>").addClass('cloud');
+    $newTag.css('font-size', (tagValue - 1) * fontIncr + fontMin + 'pt').append(tagName);
+    $tags.append($newTag);
+  });
+
+  $result.append($tags);
+  $("#main").html($result);
 
   return $result;
 }
 
 // Given a list of authors, render them into HTML
 function renderAuthors(authors) {
-  var $result;
+  var data = $.parseJSON(authors);
+  var $result = $("<div>");
+  $result.addClass('content-container');
 
+  //Create a list of authors
+  $authors = $("<p>").addClass('content');
+  $title = $("<p>").append('All credited authors: ')
+    .addClass('bold').css('margin-bottom', '1em').css('margin-top', '0em');
+  $authors.append($title);
+
+  // Loop over all authors and add their name to our new element
+  $.each(data, function(authorName, occurrences) {
+    $author = $("<p>")
+    $author.append(authorName + ' - ' + occurrences).addClass('marginless');
+    $authors.append($author);
+  });
+
+  // Append to our final result
+  $result.append($authors);
+
+  $("#main").html($result);
   return $result;
 }
 
 // Given a list of articles and their images, render them into HTML
 function renderThumbnailArticles(articles) {
-  var $result;
+  var data = $.parseJSON(articles);
+  var $result = $("<div>");
+  $result.addClass('content-container').addClass('flex-row');
 
+  // Iterate over all the articles and add them as thumbnails first
+  $.each(data, function(i, article) {
+    $article = $("<div>").addClass('img-content').addClass('blur').addClass('borderless');
+    var $link = $("<a>").addClass('img-desc');
+    $link.attr('href', article['short_url']).append(article['title']);
+    var $img = $("<img>");
+    // We have two different paths to take since some articles don't have images
+    if (article['thumbnail']) {
+      // We can retrieve information from the article
+      $img.attr('src', article['thumbnail']['url'])
+        .attr('height', article['thumbnail']['height'])
+        .attr('width', article['thumbnail']['width'])
+        .attr('alt', article['thumbnail']['caption']);
+    }
+    else {
+      // Use a placeholder image otherwise
+      $img.attr('src', '/assets/img/placeholder_thumbnail.png');
+    }
+    $article.append($img);
+    $article.append($link);
+    $result.append($article);
+  });
+
+  $("#main").html($result);
   return $result;
 }
 
 // Given a list of dates and short URLs to articles published, render them into HTML
 function renderShortURLs(dates) {
-  var $result;
+  var data = $.parseJSON(dates);
+  var $result = $("<div>");
+  $result.addClass('content-container');
+
+  // Simply loop over all the dates
+  $.each(data, function(i, date) {
+    var $date = $("<div>");
+    $date.addClass('content');
+    var $dateTitle = $("<p>")
+    $dateTitle.append("Stories submitted on " + i);
+    $date.append($dateTitle);
+    // On each date, iterate over the array of URLs and append them
+    $.each(date, function(i, url) {
+      var $url = $("<a>");
+      $url.append(url).attr('href', url).append('<br/>');
+      $date.append($url);
+    });
+    $result.append($date);
+  });
+
+  $("#main").html($result);
 
   return $result;
 }
 
 // Given a single article, render its details into HTML
 function renderDetailedArticle(article) {
-  var $result;
+  var data = $.parseJSON(article);
+  var $result = $("<div>");
+  $result.addClass('content-container');
+  $article = $("<p>").addClass('content');
 
+
+  // We can access all the relevant data entries through their keys
+  var $title = $("<p>");
+  $title.append(data['title']).css('font-weight', 'bold');
+  var $byline = $("<p>");
+  $byline.append(data['byline']);
+  var $sections = $("<p>");
+  $sections.append(data['section']);
+  // Since some articles don't have subsections we should check for one
+  if (data['subsection'].length) {
+    $sections.append(' - ' + data['subsection']);
+  }
+  var $date = $("<p>");
+  $date.append(data['published_date']).css('font-size', 'small');
+  var $abstract = $("<p>");
+  $abstract.append(data['abstract']);
+  var $short_url = $("<a>");
+  $short_url.append(data['short_url']).attr('href', data['short_url']);
+  // Like usual, append it all to the new element and that to the result
+  $article.append($title, $date, $byline, $sections, $abstract, $short_url);
+  $result.append($article);
+
+  $('#main').html($result);
   return $result;
 }
 
+function getInstructionsHTML() {
+  $.get({
+    // This should be the URL to the API you wish to access
+    url: '/instructions.html',
+    // With the appropriate request method
+    method: 'get',
+    // Then, invoke your function given some result if it succeeds
+    success: function(result) {
+      $("#main").html(result);
+    }
+  });
+}
+
+// Always make sure we wait for the document to be ready
 $(document).ready(function() {
-  $('#button_basicArticles').click(function() {
+  // Initially load the instructions when first viewing this page
+  getInstructionsHTML();
+  // Reload the instructions page any time from the footer
+  $('#instructions').click(function() {
+    getInstructionsHTML();
+  })
+  // Listen for various button clicks and then use AJAX to forward requests
+  $('#basicArticles').click(function() {
     $.getJSON({
+      // This should be the URL to the API you wish to access
       url: '/articles/basic',
+      // With the appropriate request method
       method: 'get',
+      // Then, invoke your function given some result if it succeeds
       success: function(result) {
         renderBasicArticles(result);
       }
     });
   });
-  $('#button_shortURLs').click(function() {
+  $('#shortURLs').click(function() {
     $.getJSON({
       url: '/articles/shorturl',
       method: 'get',
-      success: renderShortURLs(data)
+      success: function(result) {
+        renderShortURLs(result);
+      }
     });
   });
-  $('#button_thumbnailArticles').click(function() {
+  $('#thumbnailArticles').click(function() {
     $.getJSON({
       url: '/articles/thumbnail',
       method: 'get',
-      success: renderThumbnailArticles(data)
+      success: function(result) {
+        renderThumbnailArticles(result);
+      }
     });
   });
-  $('#button_authors').click(function() {
+  $('#authors').click(function() {
     $.getJSON({
       url: '/authors',
       method: 'get',
-      success: renderAuthors(data)
+      success: function(result) {
+        renderAuthors(result);
+      }
     });
   });
-  $('#button_tags').click(function() {
+  $('#tags').click(function() {
     $.getJSON({
       url: '/tags',
       method: 'get',
-      success: renderTags(data)
+      success: function(result) {
+        renderTags(result);
+      }
     });
   });
-  $('#button_detailedArticle').click(function() {
+  $('#detailedArticle').click(function() {
     $.getJSON({
-      url: '/articles/thumbnail',
+      url: '/articles/' + $('#article_index').val(),
       method: 'get',
-      success: renderDetailedArticle(data)
+      success: function(result) {
+        renderDetailedArticle(result);
+      }
     });
   });
 });
--- readme.txt
+++ readme.txt
@@ -4,6 +4,8 @@ Daniil Kouznetsov, g3dakouz, daniil.kouznetsov@mail.utoronto.ca
 RESTful API:
 --------------------------------------------------
 GET /articles/basic
+Get all articles with only basic data for each article
+
 returns:
   [{'published_date' : 'Date of Publication',
     'title' : 'The Title of the Article',
@@ -14,6 +16,11 @@ returns:
 
 --------------------------------------------------
 GET /articles/thumbnail
+Get all articles with basic data and a corresponding thumbnail
+
+Note: if there is no thumbnail for a given article then the
+  'thumbnail' field will not be initialized
+
 returns:
   [{'published_date' : 'Date of Publication',
     'title' : 'The Title of the Article',
@@ -24,8 +31,8 @@ returns:
         "format": "Standard Thumbnail",
         "height": 75,
         "width": 75,
-        "type": "type of thumbnail",
-        "subtype": "subtype of thumbnail",
+        "type": "type of picture (e.g. photo)",
+        "subtype": "subtype of thumbnail (e.g. panorama)",
         "caption": "A brief description of the image",
         "copyright": "Copyright information associated with the image"
         }
@@ -34,12 +41,18 @@ returns:
 
 --------------------------------------------------
 GET /articles/shorturl
+Get all articles grouped by published data and represented by shortened URL
+
 returns:
-  [{'published_date' : ['date1', 'date2', ...]},
+  [{'published_date1' : ['url1', 'url2', ...]},
     ...]
 
 --------------------------------------------------
 GET /articles/###
+Get detailed information about a particular article given the index as it is
+  stored in JSON locally
+Note: some articles do not have a subsectio and the field will return empty
+
 returns:
   {'section' : 'The main section this article is under',
    'subsection' : 'A more specific section this article fits into',
@@ -52,17 +65,20 @@ returns:
 
 --------------------------------------------------
 GET /authors
+Get all authors and the number of articles they have written
+
 returns:
-  ['AUTHOR ONE',
-   'AUTHOR TWO',
-   'AUTHOR THREE',
-   'AUTHOR FOUR',
-   ...]
+  {'AUTHOR ONE' : 2,
+   'AUTHOR TWO' : 1,
+   'AUTHOR THREE' : 5723,
+   ...}
 
 --------------------------------------------------
 GET /tags
+Get all tags and their occurrences throughout all articles
+
 returns:
- ['Tag1' : 'Occurrences of Tag1',
-  'Tag2' : 'Occurrences of Tag2',
-  'Tag3' : 'Occurrences of Tag3',
-  ...]
+ {'Tag1' : 2,
+  'Tag2' : 20,
+  'Tag3' : 9001,
+  ...}
